5d21372ffa704b38a19c6be1b7879eae379d7085
Matheus Papa <matheusalmeida13.ma@gmail.com>
2021-11-30 23:23:20 -0400

    Keylogger client finalizado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import file from '../../../KeyLogger/keys.txt';
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    readFile();
+  }, [text]);
+
+  function readFile() {
+    fetch(file)
+    .then(r => r.text())
+    .then(text => {
+      setText(text);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Texto do arquivo:</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
